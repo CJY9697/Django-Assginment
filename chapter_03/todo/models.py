c8c55937b2f6e35852cb8488368b007355f2d4e3
CJY9697 <jy970114@naver.com>
2025-05-09 17:07:42 +0900

    chapter_04 과제 업로드

--- chapter_03/todo/models.py
+++ chapter_03/todo/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Todo(models.Model):
-    DoesNotExist = None
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.TextField()
     start_date = models.DateTimeField()
